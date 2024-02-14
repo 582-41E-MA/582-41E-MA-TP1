4b27aed456a1538be98841fa97a6ad8717430536
Yu <e2296635@cmaisonneuve.qc.ca>
2024-02-14 15:06:50 -0500

    init stripe #14

--- app/Http/Controllers/ExerciceController.php
+++ app/Http/Controllers/ExerciceController.php
@@ -36,4 +36,23 @@ class ExerciceController extends Controller
     //     //return $request;
     //     return view('contact', ['data' => $request]);
     // }
+
+    /**add to test stripe */
+    public function stripePost(Request $request){
+        //code copied from: https://docs.stripe.com/payments/checkout/how-checkout-works?ui=stripe-hosted
+        $stripe = new \Stripe\StripeClient('sk_test_Hrs6SAopgFPF0bZXSN3f6ELN');
+
+        $stripe->checkout->sessions->create([
+            'line_items' => [
+                [
+                    'price' => '1',
+                    'quantity' => 1, //this can be a parameter obtained from the url
+                ],
+            ],
+            'mode' => 'subscription',
+            'success_url' => 'pay/success',
+            'cancel_url' => 'pay/cancel',
+        ]);
+
+    }
 }
