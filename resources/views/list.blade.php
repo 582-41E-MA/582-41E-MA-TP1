44826e5f9f937f8f603e67ba5c722cbbcc60ac19
Your Name <pwdawson90@gmail.com>
2024-02-15 16:58:23 -0500

    premier commmit sur ma branche. nouveau css. font

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,7 +3,29 @@
 <!-- pour le titre dynamique (au lei de section/endesction, definir variable string direct-->
 @section('title', 'List')
 
+@section('hero')
+<div class="hero">
+<div class="container-fluid d-flex align-items-center">
+  <div class="row justify-content-center w-100">
+    <div class="col">
+      <div class="p-3 border text-white"><h1>The Cat Store</h1></div>
+    </div>
+    <div class="col">
+      <div class="p-3 border">logo</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border">Attack curtains poop in litter box, scratch the
+walls for kitty slap the dog because cats rule lick
+yarn hanging out of own butt but why can't i catch that
+stupid red dot leave fur on owners clothes.</div>
+    </div>
+  </div>
+</div>
+</div>
+@endsection
 @section('content')
+
+
 <div>
 
 ici il y aura la list genere dynamiquement pour afficher la liste des produits
