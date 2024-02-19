bc506dd5489166782687e640e3c732d3edfd5df7
Your Name <pwdawson90@gmail.com>
2024-02-18 19:38:01 -0500

    list,title

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -27,14 +27,50 @@ stupid red dot leave fur on owners clothes.</div>
 
 
 
+
+
+
 <!-- Experience Card 1-->
 <a href="show" class="text-decoration-none text-reset">
   <div class="card shadow border-0 rounded-4 my-3 mx-5">
     <div class="card-body p-5">
       <div class="row align-items-center gx-5">
-        <div class="col text-center text-lg-start mb-4 mb-lg-0">
-          <div class="bg-light p-4 rounded-4">
-            <img src="{{ asset('images/catstorelogo.jpg' )}}" alt="" style="width:100px;">
+        <div class="col text-center bg-light p-4 rounded-4  text-lg-start mb-4 mb-lg-0 content-wrapper">
+          <div class="image-wrapper">
+            <img src="{{ asset('images/catstorelogo.jpg') }}" alt="" style="width:150px;">
+          </div>
+          <div>
+            <p>Nom du chat</p>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex justify-content-end">
+          <button class="btn btn-primary">Ajouter au Panier</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</a>
+
+
+
+<!-- Experience Card 2-->
+<a href="show" class="text-decoration-none text-reset">
+  <div class="card shadow border-0 rounded-4 my-3 mx-5">
+    <div class="card-body p-5">
+      <div class="row align-items-center gx-5">
+        <div class="col text-center bg-light p-4 rounded-4  text-lg-start mb-4 mb-lg-0 content-wrapper">
+          <div class="image-wrapper">
+            <img src="{{ asset('images/catstorelogo.jpg') }}" alt="" style="width:150px;">
+          </div>
+          <div>
+            <p>Nom du chat 2</p>
           </div>
         </div>
         <div class="col-lg-8">
